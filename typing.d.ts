3883327646be9d673f66d6756c405559be887e10
庄俊霖 <zhuangjunlin@hupu.com>
2022-06-17 22:00:02 +0800

    fix: .d.ts

--- typing.d.ts
+++ typing.d.ts
@@ -1,3 +1,17 @@
-export type Case = 'kebab' | 'upperCamelCase' | 'lowerCamelCase';
+export type Case = 
+  | 'camel-case'
+  | 'lower-camel-case'
+  | 'capital-case'
+  | 'constant-case'
+  | 'dot-case'
+  | 'header-case'
+  | 'no-case'
+  | 'param-case'
+  | 'kebab'
+  | 'pascal-case'
+  | 'upper-camel-case'
+  | 'path-case'
+  | 'sentence-case'
+  | 'snake-case';
 declare function changeCase(s: string, _case?: Case): string;
 export default changeCase;
